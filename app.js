57cafaeaeaec28adf38e1a4c6932ce0b49361d1a
umairsolangi <51870556+umairsolangi@users.noreply.github.com>
2020-07-24 19:09:10 +0500

    Dark Theme Mode

---
+++ app.js
@@ -0,0 +1,32 @@
+// function changeTheme(){
+//     document.body.style.backgroundColor = "black";
+//     document.getElementsByTagName("h2").style.color = "#ffffff";
+//     document.getElementById("")
+// }
+
+
+// var changeColorId;
+// function init() { 
+//     changeColorId = document.getElementById("changeColorId").innerHTML;
+//     changeColorIdb.style.color = 'blue';
+// }
+
+function light() { 
+    document.body.style.backgroundColor = "black";
+    document.getElementById("about").style.color = 'yellow';
+    document.getElementById("about2").style.color = 'yellow';
+    document.getElementById("about3").style.color = 'yellow';
+    document.getElementById("headertext").style.color = 'yellow';
+    document.getElementById("pera1").style.color = 'white';
+    document.getElementById("pera2").style.color = 'white';
+    document.getElementById("pera3").style.color = 'white';
+    document.getElementById("logo").style.color = 'white';
+    document.getElementById("headerr").style.backgroundColor = 'black'; 
+    document.getElementById("dark-mod").style.display = "none";
+    document.getElementById("light-mod").style.display = "block";
+
+
+  }
+  function dark(){
+    window.location = "index.html";
+  }
